d8ef8e0f893304caba097f120703e0a256c52d12
02 <96946615+AnitaSherry@users.noreply.github.com>
2024-03-06 06:08:24 +0000

    图片向量化

---
+++ Image_vectorization.py
@@ -0,0 +1,50 @@
+import os
+import argparse
+from PIL import Image, ImageSequence
+from resnet101_embding.embding import resnet_embeding
+from milvus_manage.milvus_operator import MilvusOperator
+
+ 
+def update_image_vector(data_path, operator: MilvusOperator):
+    idxs, embedings, paths = [], [], []
+ 
+    total_count = 0
+    for dir_name in os.listdir(data_path):
+        file = os.path.join(data_path, dir_name)
+
+        embeding = resnet_embeding(file)
+
+        idxs.append(total_count)
+        embedings.append(embeding[0].detach().numpy().tolist())
+        paths.append(file)
+        total_count += 1
+
+        if total_count % 50 == 0:
+            data = [idxs, embedings, paths]
+            operator.insert_data(data)
+
+            print(f'success insert {operator.coll_name} items:{len(idxs)}', end="\r")
+            idxs, embedings, paths = [], [], []
+ 
+        if len(idxs):
+            data = [idxs, embedings, paths]
+            operator.insert_data(data)
+            print(f'success insert {operator.coll_name} items:{len(idxs)}', end="\r")
+ 
+    print(f'finish update {operator.coll_name} items: {total_count}')
+ 
+ 
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--host", default="192.168.10.60", help="本机IP地址")
+    parser.add_argument("--database_name", default="image_vector_db_windows", help="milvus数据库名称")
+    parser.add_argument("--collection_name", default="antique_vector_windows", help="milvus集合名称")
+    parser.add_argument("--metric_type", choices=["L2","IP","JACCARD"],default="L2", help="度量类型")
+    parser.add_argument("--data", default="data", help="milvus集合名称")
+
+    args = parser.parse_args()
+
+    antique_image = MilvusOperator(args.host, args.database_name, args.collection_name, args.metric_type)
+    update_image_vector(args.data_dir,antique_image)
+ 
