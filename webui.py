660e85722174e644f61176eade1f6b586e6f37e5
02 <96946615+AnitaSherry@users.noreply.github.com>
2024-03-06 08:05:23 +0000

    web端展示

--- webui.py
+++ webui.py
@@ -6,8 +6,6 @@ from resnet101_embding.embding import resnet_embeding
 from milvus_manage.milvus_operator import MilvusOperator
 import time
 
-blank_ = Image.new("RGB", (215, 300))
-
 def image_search(image):
     # start_time_0 = time.time()
     if image is None:
@@ -26,23 +24,16 @@ def image_search(image):
     # print("查询总时间：", time.time()-start_time_0, "秒")
     return pil_images
 
-    
-def calculate_rows_and_cols(num_images):
-    sqrt_num_images = int(num_images ** 0.5)
-    num_cols = sqrt_num_images
-    num_rows = num_images // num_cols
-    if num_images % num_cols != 0:
-        num_rows += 1
-    return num_rows, num_cols
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     parser.add_argument("--host", default="192.168.10.60", help="本机IP地址") # 192.168.10.60
+    parser.add_argument("--server_port", default=9090, help="port端口号")
     parser.add_argument("--database_name", default="image_vector_db_windows", help="milvus数据库名称")
     parser.add_argument("--collection_name", default="antique_vector_windows", help="milvus集合名称")
     parser.add_argument("--metric_type", choices=["L2","IP","JACCARD"],default="L2", help="度量类型")
     parser.add_argument("--limit", default=4, help="搜索topk")
+    
 
     args = parser.parse_args()
 
@@ -62,4 +53,4 @@ if __name__ == "__main__":
     )
 
     ip_addr = net_helper.get_host_ip()
-    app.queue(concurrency_count=8).launch(show_api=False, share=True, server_name=ip_addr, server_port=9090)
+    app.queue(concurrency_count=8).launch(show_api=False, share=True, server_name=ip_addr, server_port=args.server_port)
